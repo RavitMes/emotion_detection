96617d718091dcacc9de6e8eacdc8614c52eccfc
Ollolly <hazin.olga@gmail.com>
2020-01-29 10:20:35 +0200

    text_sentiment.py + conf file

---
+++ conf.py
@@ -0,0 +1,1 @@
+GOOGLE_APPLICATION_CREDENTIALS_PATH = 'path_to_your_.json_credential_file'
---
+++ text_sentiment.py
@@ -0,0 +1,12 @@
+"""
+TO DO
+"""
+
+import os
+from conf import GOOGLE_APPLICATION_CREDENTIALS_PATH
+
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
