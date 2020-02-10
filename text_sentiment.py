980dff955bb8e66f8d229c9f232413f78929f4b7
Ollolly <hazin.olga@gmail.com>
2020-02-10 12:57:33 +0200

    finished class TextSentiment

--- text_sentiment.py
+++ text_sentiment.py
@@ -4,14 +4,19 @@ TO DO
 The score of a document's sentiment indicates the overall emotion of a document.
 The magnitude of a document's sentiment indicates how much emotional content is present within the document,
 and this value is often proportional to the length of the document.
+    - score range is between -1.0 (negative) and 1.0 (positive).
+    - magnitude rangeg is between 0.0 and +inf.
+For more information: https://cloud.google.com/natural-language/docs/basics#interpreting_sentiment_analysis_values
 
 For list of supported languages: https://cloud.google.com/natural-language/docs/languages
 
 """
 
 import os
-from conf import GOOGLE_APPLICATION_CREDENTIALS_PATH
+import requests
 from bs4 import BeautifulSoup
+
+from conf import GOOGLE_APPLICATION_CREDENTIALS_PATH
 
 # Imports the Google Cloud client library
 from google.cloud import language
@@ -24,22 +29,26 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS_PA
 
 
 class TextSentiment:
-    # type = enums.Document.Type.HTML
-    _type = enums.Document.PLAIN_TEXT
+    _sent_label = 0
+    _sent_score = 1
 
     def __init__(self):
         self.client = language.LanguageServiceClient()
+        self.sentiment = [('negative', -0.3), ('Neutral', 0.3), ('positive', 1)]
+        self.doc_type = 'HTML'
 
-    def parse_html(self, source_text):
+    @staticmethod
+    def _get_url_text(url):
         """
-        TO DO
-        :param source_text: The text content to analyze
-        :type source_text:
+        The function receives the URL and extracts text from it.
+        :param url:
         :return:
         :rtype:
         """
-        soup = BeautifulSoup(source_text)  # TO DO check input form
-        return soup.text
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.text, 'lxml')
+        data = soup.get_text()
+        return data
 
     def analyze(self, text):
         """
@@ -50,23 +59,32 @@ class TextSentiment:
         :rtype: tuple(float, float)
         """
         # text prep
-        document = types.Document(content=text, type=self._type)
+        document = types.Document(content=text, type=self.doc_type)
         # Detects the sentiment of the text
         annotations = self.client.analyze_sentiment(document=document).document_sentiment
-        sentiment_score = annotations.document_sentiment.score
-        magnitude = annotations.document_sentiment.magnitude
+        sentiment_score = annotations.score
+        magnitude = annotations.magnitude
         return sentiment_score, magnitude
 
+    def get_sentiment(self, url):
+        """
+        Function gets url and calculates sentiment score of the web page content
+        :return: the label of the sentiment score
+        :rtype: string
+        """
+        sentiment = ''
+        try:
+            text = self._get_url_text(url)
+            sentiment_score, _ = self.analyze(text)
+            for s_score in self.sentiment:
+                if sentiment_score < s_score[self._sent_score]:
+                    sentiment = s_score[self._sent_label]
+                    break
+            print(sentiment_score)
 
+        except requests.exceptions.HTTPError as err:
+            print(f'Error: {err}')
+        except Exception as err:
+            print(f'Error: {err}')
 
-
-
-
-
-
-
-
-
-
-
-
+        return sentiment
